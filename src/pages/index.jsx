dbf0530a8690f593a8d1742997c9b206814e9da4
Roman Braverman <romanbr87@gmail.com>
2025-10-23 21:54:04 +0300

    Update index.jsx
    More correct way to write the code,
    although the result is the same

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,9 +15,9 @@ const Home = () => {
                         או למצב צפייה כדי להציג את הרשימה הנוכחית.
                     </p>
                     <hr className="my-4" />
-                    <Link href="/shoppinglistapp" passHref>
-                        <Button variant="primary" size="lg">התחל עכשיו</Button>
-                    </Link>
+                    <Button as={Link} href="/shoppinglistapp" variant="primary" size="lg">
+                        התחל עכשיו
+                    </Button>
                 </div>
             </Container>
         </>
