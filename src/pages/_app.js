07b019d2962487c876b817d45367962845c0eb1e
Roman Braverman <romanbr87@gmail.com>
2026-04-26 02:16:03 +0300

    Fix for _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 // pages/_app.js
 import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import '@/styles/globals.css'
 import '@/styles/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const App = ({ Component, pageProps }) => {
     const title = "רשימהלי - ניהול רשימות קנייה בקלות";
